19d6c092c8e21d707ef63b6d6c6a86a490b2f3e6
Balestrini, Santiago <Santiago.Balestrini@gtri.gatech.edu>
2015-09-12 18:24:38 -0400

    moved version to 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_reqs = parse_requirements('requirements.txt', session=PipSession())
 requirements = [str(ir.req) for ir in install_reqs]
 
 setup(name='assist',
-      version='0.0.2',
+      version='0.0.3',
       description='Aircraft Sizing, Synthesis and Integration Support Tool (ASSIST)',
       long_description=open('README.md').read(),
       download_url='https://github.com/sanbales/ASSIST',
