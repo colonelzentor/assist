58135d255f48458ca8d53d1449bf038dabe08229
Balestrini, Santiago <Santiago.Balestrini@gtri.gatech.edu>
2015-09-12 19:18:23 -0400

    fleshing out the testing

--- assist/test/notebooks.py
+++ assist/test/notebooks.py
@@ -1,15 +1,21 @@
 import os
 import sys
 from unittest import TestCase
-import runipy
+
+from runipy.notebook_runner import NotebookRunner
+from IPython.nbformat.current import read
 
 
-
-class Development(TestCase):
+class Notebooks(TestCase):
+    self.DEVELOPMENT = 'notebooks/development.ipynb'
+    self.TUTORIAL = 'notebooks/tutorial.ipynb'
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test_notebook(self):
+    def test_development(self):
+        notebook = read(open(self.NOTEBOOK), 'json')
+        r = NotebookRunner(notebook, pylab=True)
+        r.run_notebook()
