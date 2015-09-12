b7dc260e606dcca7181b861a14b328d6e0f0a410
Balestrini, Santiago <Santiago.Balestrini@gtri.gatech.edu>
2015-09-12 19:10:15 -0400

    added relative import of assist in the ipython notebook

---
+++ assist/test/notebooks.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from unittest import TestCase
+import runipy
+
+
+
+class Development(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_notebook(self):
